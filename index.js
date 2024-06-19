e819263419a2afb9b02ba396d8c15297379085d6
BuhleMtshali <mtshalizanobuhle7@gmail.com>
2024-06-20 01:10:25 +0200

    updated the js file

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const inputElement = document.querySelector("#input-element");
 const listContainer = document.querySelector("#list-container");
-const listArray = [];
+let listArray = [];
+let listFromStorage = JSON.parse(localStorage.getItem("tasks"));
+if (listFromStorage) {
+  listArray = listFromStorage;
+  renderItems();
+}
 
 function addTask() {
   listArray.push(inputElement.value);
   inputElement.value = "";
+  localStorage.setItem("tasks", JSON.stringify(listArray));
+  renderItems();
+}
+
+function renderItems() {
+  let itemsList = "";
+  for (let i = 0; i < listArray.length; i++) {
+    itemsList += `<li> ${listArray[i]}</li>`;
+  }
+  listContainer.innerHTML = itemsList;
 }
 
 const addButton = document.querySelector("#add-item");
