75b6e0908f09e6cd7bdbb6290f30be7cd95d9c3a
BuhleMtshali <mtshalizanobuhle7@gmail.com>
2024-06-20 01:34:27 +0200

    updated the javascript file

--- index.js
+++ index.js
@@ -21,9 +21,20 @@ function renderItems() {
   let itemsList = "";
 
   for (let i = 0; i < listArray.length; i++) {
-    itemsList += `<li> ${listArray[i]}</li>`;
+    itemsList += `<li> ${listArray[i]} <button class="delete-btn" data-index="${i}">Delete</button></li>`;
   }
   listContainer.innerHTML = itemsList;
+  let deleteButtons = document.querySelectorAll(".delete-btn");
+  deleteButtons.forEach((button) => {
+    button.addEventListener("click", deleteItem);
+  });
+}
+
+function deleteItem(event) {
+  let index = event.target.getAttribute("data-index");
+  listArray.splice(index, 1);
+  localStorage.setItem("tasks", JSON.stringify(listArray));
+  renderItems();
 }
 
 function clearAll() {
